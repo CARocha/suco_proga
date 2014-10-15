9205f16be72e3264a914f93d9ec104dc6299b3ba
Simon Vézina <svezina@tribalsolutions.ca>
2014-10-14 21:37:59 -0600

    Revert "Revert "Commit backup - instalando nueva computadora""
    This reverts commit b9d01c50ca36dc5c559c451de8504953e870102e.

--- suco/encuesta/forms.py
+++ suco/encuesta/forms.py
@@ -2,10 +2,25 @@
 from django import forms
 from django.forms import ModelForm
 from suco.encuesta.models import *
+from suco.jovenes.models import *
 from suco.lugar.models import *
 from suco.finca_tierra.models import *
 
-CHOICE_SEXO = (('', u'Sexo'),(1,'Hombre'),(2,'Mujer'))
+CHOICE_SEXO = (('', u'Sexo'),(1,'Hombre'),(2,'Mujer')) #CUIDADO -> los informes viejas tenian 1 como Hombre et 2 como Mujer, et es el contrario.
+
+CHOICE_ENCUESTA_NUM = (
+    ('1','Primera encuesta'),
+    ('2','Segunda encuesta'),
+    ('3','Comparar las dos encuestas'),
+    )
+CHOICE_INFORME_TIPO = (
+    ('','Qué tipo de informes?'),
+    ('informes_originales','Informes originales'),
+    ('informes_nuevos','Nuevos informes'),
+    )
+CHOICE_INFORME_INDICADOR = (
+    ('aumento_de_la_produccion','% de aumento de la producción'),
+    )
 
 def get_anios():
     choices = []
@@ -16,7 +31,13 @@ def get_anios():
         choices.append((year, year))
     return choices
 
+#El formulario original ariba
 class MonitoreoForm(forms.Form):
+
+    #escojer entre tipos de informes (originales y nuevos)
+    informe_tipo = forms.ChoiceField(choices = CHOICE_INFORME_TIPO, required=True)
+
+    #Informes originales
     fecha = forms.ChoiceField(choices=get_anios(), label="Años")
     departamento = forms.ModelChoiceField(queryset=Departamento.objects.all(), 
             required=False, empty_label="Departamento")
@@ -26,3 +47,15 @@ class MonitoreoForm(forms.Form):
     dueno = forms.ModelChoiceField(queryset=Documento.objects.all(), 
             required=False, empty_label="Dueños")
 #    dueno = forms.ChoiceField(label = 'Dueño', choices = CHOICE_DUENO_F , required=False, initial=u"Dueño")
+
+    #Nuevos informes - Octubre 2014
+    grupo = forms.ModelChoiceField(queryset=Grupo.objects.all(),
+            required=False, empty_label="Todos los grupos")
+
+    numero_encuesta = forms.ChoiceField(choices = CHOICE_ENCUESTA_NUM, required=True)
+    indicador = forms.ChoiceField(choices = CHOICE_INFORME_INDICADOR, required=True)
+    jovenes_activados_solo = forms.BooleanField(required=False)
+
+
+
+
