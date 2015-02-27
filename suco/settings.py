701b1a458ee592da7173ad022bae56a0ea47ce7f
Simon Vézina <svezina@tribalsolutions.ca>
2015-02-27 15:54:40 -0600

    configs

--- suco/settings.py
+++ suco/settings.py
@@ -13,7 +13,7 @@ MEDIA_URL = '/files/uploads/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/public_html/linea/files/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
